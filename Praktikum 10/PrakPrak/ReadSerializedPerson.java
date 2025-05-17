1c573bdf851ac17adc5debe14dbb67b25f659b23
JooGGgemink <163154311+JooGGgemink@users.noreply.github.com>
2025-05-17 13:46:10 +0700

    Add files via upload

---
+++ Praktikum 10/PrakPrak/ReadSerializedPerson.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class ReadSerializedPerson {
+    public static void main(String[] args) {
+        Person p = null;
+        try {
+            FileInputStream s = new FileInputStream("person.ser");
+            ObjectInputStream f = new ObjectInputStream(s);
+            p = (Person) f.readObject();
+            System.out.println("kelar baca person.ser");
+            System.out.println("Nama: " + p.getName());
+            f.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+    }
+}
