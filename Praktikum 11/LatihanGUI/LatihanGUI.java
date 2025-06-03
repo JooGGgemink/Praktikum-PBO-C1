d212924a5ced03d2e7bbc781edb7d2a995f01af3
JooGGgemink <163154311+JooGGgemink@users.noreply.github.com>
2025-06-03 20:42:52 +0700

    Add files via upload

---
+++ Praktikum 11/LatihanGUI/LatihanGUI.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+ /**
+  *
+  * @author jo_ha
+  */
+ public class LatihanGUI {
+ 
+     public static void main(String[] args) {
+         System.out.println("Hello World!");
+     }
+ }
+ 
